aa71fcee2d98ea27f9ab82f8c40db2b8c42c51ab
Pavand46 <153343892+Pavand46@users.noreply.github.com>
2025-08-28 16:58:47 +0530

    Update Main.java

--- 9.HQL Delete/Main.java
+++ 9.HQL Delete/Main.java
@@ -1,1 +1,38 @@
 
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        SessionFactory sf =  new Configuration()
+                .addAnnotatedClass(org.example.Student.class)
+                .configure()
+                .buildSessionFactory();
+
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        Query query = session.createQuery("delete from Student where id=?1");
+        query.setParameter(1,105);
+
+        int rowsAffected = query.executeUpdate();
+
+        transaction.commit();
+
+        System.out.println("Rows affected = "+rowsAffected);
+
+
+        session.close();
+        sf.close();
+
+
+    }
+}
